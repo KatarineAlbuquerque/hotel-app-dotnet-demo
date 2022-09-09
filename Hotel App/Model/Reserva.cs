fa6c052297efbd983860a330cd1323b90527a444
KatarineAlbuquerque <katarine.kba@gmail.com>
2022-09-09 17:46:03 -0300

    Hotel App .Net

---
+++ Hotel App/Model/Reserva.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_App.Model
+{
+    internal class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            // Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
+            if (Suite.Capacidade >= hospedes.Count)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                throw new ArgumentException("A capacidade é menor que o número de hóspedes recebidos!");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            // Retorna o valor da diária
+            // Cálculo: DiasReservados X Suite.ValorDiaria
+            decimal valor = 0;
+            valor = DiasReservados * Suite.ValorDiaria;
+
+            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
+            if (DiasReservados >= 10)
+            {
+                decimal desconto = DiasReservados - 1.00M;
+                valor = desconto * Suite.ValorDiaria;
+            }
+            return valor;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+<div align="center">
+
+# Hotel App
+## .NET
+
+##### Trabalho realizado para apresentação de suas metodologias e aplicações através da didática da Digital Innovation One (DIO).
+
+##### Aplicativo que simula a entrada de hóspedes em um hotel.
+
+## Tecnologias Utilizadas
+
+##### Visual Studio IDE
+##### .NET 6
+##### C#
+
+## Autor
+##### Katarine Albuquerque
+##### Linkedin: <a href="https://www.linkedin.com/in/katarine-albuquerque/">/katarine-albuquerque</a>
+##### GitHub: <a href="https://github.com/KatarineAlbuquerque">/KatarineAlbuquerque</a>
+##### Portfólio GitHub: <a href="https://github.com/KatarineAlbuquerque/my-portfolio">/my-portfolio</a>
+
+</div>
